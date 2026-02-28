0e9515d3172857373be95cf47a87f2f9578f1b31
Yawniverse <jan.bozerocki@gmail.com>
2026-02-27 21:49:50 -0800

    Update main.c
    yellow and red

--- Src/main.c
+++ Src/main.c
@@ -1325,7 +1325,7 @@ void tenKhzRoutine()
                             send_LED_RGB(0, 255, 0);
 #endif
 #ifdef USE_RGB_LED
-                            setIndividualRGBLed(0,0,1);
+                            setIndividualRGBLed(1,1,0);
 #endif
                             if ((cell_count == 0) && eepromBuffer.low_voltage_cut_off == 1) {
                                 cell_count = battery_voltage / 370;
